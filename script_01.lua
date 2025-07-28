af75426fa7ee9d44180860b2329a76afb3728e28
zSousaxiz <ssousa2467@gmail.com>
2025-07-28 16:01:41 -0300

    update script_01.lua

--- script_01.lua
+++ script_01.lua
@@ -1,38 +1,42 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
 
-local Window = OrionLib:MakeWindow({Name = "Teste", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest", IntroEnabled = false})
+local Window = redzlib:MakeWindow({
+  Title = "Six Hub - Anime Boss Raid ⚔️",
+  SubTitle = "by X",
+  SaveFolder = nil
+})
 
+Window:AddMinimizeButton({
+    Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(60, 60) },
+})
 
 -- Tab Main
-local Main = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
+local Main = Window:MakeTab({"Main", "home"})
 
 -- Auto Summon Packs
 Main:AddToggle({
-	Name = "Auto Summon Packs",
-	Default = false,
-	Callback = function(Value)
-		print(Value)
-	end    
+    Name = "Auto Summon Packs",
+    Default = false,
+    Callback = function(v)
+    	
+    end
 })
 
 -- Auto Sell
 Main:AddToggle({
-	Name = "Auto Sell",
-	Default = false,
-	Callback = function(Value)
-		print(Value)
-	end    
+    Name = "Auto Sell",
+    Default = false,
+    Callback = function(v)
+    	
+    end
 })
 
 -- Auto Buy Gold Shop
 Main:AddToggle({
-	Name = "Auto Buy Gold Shop",
-	Default = false,
-	Callback = function(Value)
-		print(Value)
-	end    
+    Name = "Auto Buy Gold Shop",
+    Default = false,
+    Callback = function(v)
+    	
+    end
 })
