98a29e492214e4c85040703d440d152e04f37a01
zSousaxiz <ssousa2467@gmail.com>
2025-07-28 11:44:07 -0300

    update script_01.lua

---
+++ script_01.lua
@@ -0,0 +1,9 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Teste", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
