7267ddc7d6fed7c2ab67cf5504b0118ef7a7d3d7
zSousaxiz <ssousa2467@gmail.com>
2025-07-28 15:56:49 -0300

    update script_01.lua

--- script_01.lua
+++ script_01.lua
@@ -1,9 +1,38 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Teste", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Teste", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest", IntroEnabled = false})
 
-local Tab = Window:MakeTab({
-	Name = "Tab 1",
+
+-- Tab Main
+local Main = Window:MakeTab({
+	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
+})
+
+-- Auto Summon Packs
+Main:AddToggle({
+	Name = "Auto Summon Packs",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
+-- Auto Sell
+Main:AddToggle({
+	Name = "Auto Sell",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
+-- Auto Buy Gold Shop
+Main:AddToggle({
+	Name = "Auto Buy Gold Shop",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
 })
